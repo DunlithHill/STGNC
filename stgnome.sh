ca8288b6ef0ed659d887cc45247041af6102b20d
Deren <deren.hansen@gmail.com>
2016-09-21 12:18:38 -0600

    Cleaned up garbled access token in last commit.

--- stgnome.sh
+++ stgnome.sh
@@ -1,5 +1,5 @@
 
-curl -i "https://api.github.com/orgs/DunlithHill/members?access_token=917b34975$
+curl -i "https://api.github.com/orgs/DunlithHill/members?access_token=917b3497597441cbacc9f7cc6516c3b3cf6809af"$
 
 curl -i "https://api.github.com/users/derenhansen?access_token=917b3497597441cbacc9f7cc6516c3b3cf6809af"
 curl -i "https://api.github.com/users/testymctestington?access_token=917b3497597441cbacc9f7cc6516c3b3cf6809af"
