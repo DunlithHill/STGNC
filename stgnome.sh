06f1f768a86b18253255ab0c7c301f229f53be90
Deren <deren.hansen@gmail.com>
2016-09-21 12:15:56 -0600

    Confirmed basic access pattern.

---
+++ stgnome.sh
@@ -0,0 +1,7 @@
+
+curl -i "https://api.github.com/orgs/DunlithHill/members?access_token=917b34975$
+
+curl -i "https://api.github.com/users/derenhansen?access_token=917b3497597441cbacc9f7cc6516c3b3cf6809af"
+curl -i "https://api.github.com/users/testymctestington?access_token=917b3497597441cbacc9f7cc6516c3b3cf6809af"
+
+
